4dd9ee9c5077bbf9990954fc007e27b63f33ba38
J-0-N-A-T-H-A-N <jonathan_garvin@hotmail.com>
2022-02-17 16:02:22 +0000

    adding comment to stop creating DB

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ class Comment(db.Model):
     post_id = db.Column(db.Integer, db.ForeignKey("blog_posts.id"))
     parent_post = relationship("BlogPost", back_populates="comments")
 
-db.create_all()
+# db.create_all()
 
 login_manager = LoginManager()
 login_manager.init_app(app)
